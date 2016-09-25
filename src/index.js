b244af099c716f420c536c102521b9f43cdf112a
Chinga Chaguluka <chinga.e.chaguluka@gmail.com>
2016-09-25 22:27:50 +0200

    Add import statement to and ReactDOM render

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+//Create a component. THis component should produce some html
+
+const App = function () {
+    return (
+        <div>Hi There </div>
+    );
+};
+
+//Render the component on the DOM
+ReactDOM.render(App);
