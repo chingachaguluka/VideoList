8ecd94938f027810272cc914c761c0d4dd153f07
Chinga Chaguluka <chinga.e.chaguluka@gmail.com>
2016-10-01 21:41:28 +0200

    Refactored class using arrow notation and remove onInputChange function

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,13 +4,9 @@ import React, {Component} from 'react';
 class SearchBar extends Component {
     render() {
         return (
-            <input onChange={this.onInputChange} />
+            <input onChange={ (event) => console.log(event.target.value) } />
         );
     }
-
-    onInputChange(event) {
-        console.log(event.target.value);
-    }
-
 }
+
 export default SearchBar;
