4716b916da97df633fa5e7f4936dbc377bfb5c8c
Chinga Chaguluka <chinga.e.chaguluka@gmail.com>
2016-10-01 22:02:39 +0200

    Initialise state with term property in constructor and assign term property to the current value of the searchbar and display the result on page

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,9 +2,22 @@ import React, {Component} from 'react';
 
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+    
     render() {
         return (
-            <input onChange={ event => console.log(event.target.value) } />
+            <div>
+                <input 
+                    onChange={ event => this.setState({ term: event.target.value }) }     
+                />
+                <p> 
+                    The value of serch is: {this.state.term}
+                </p> 
+            </div> 
         );
     }
 }
