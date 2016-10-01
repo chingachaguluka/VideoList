54510e65469664369e70f875696eb7236d3c865e
Chinga Chaguluka <chinga.e.chaguluka@gmail.com>
2016-10-01 21:43:24 +0200

    Removed the parentesis in arrow expression

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react';
 class SearchBar extends Component {
     render() {
         return (
-            <input onChange={ (event) => console.log(event.target.value) } />
+            <input onChange={ event => console.log(event.target.value) } />
         );
     }
 }
