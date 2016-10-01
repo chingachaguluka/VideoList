d0c7e557b947a23ec8cdfcfda1650b27098e75c8
Chinga Chaguluka <chinga.e.chaguluka@gmail.com>
2016-10-01 21:37:10 +0200

    Add input event handler for onChange

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,9 +4,13 @@ import React, {Component} from 'react';
 class SearchBar extends Component {
     render() {
         return (
-            <input />
+            <input onChange={this.onInputChange} />
         );
-
     }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+
 }
 export default SearchBar;
