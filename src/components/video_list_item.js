87106e17c878561e4e3257a235a54e0d7e9c70e3
Chinga <royalse-me@yahoo.com>
2016-10-12 17:26:13 +0200

    Fix the paarameter for video_list_item to resolve issue of vidoes not displaying in the list

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 
-const VideoListItem = (video) => {
-  //const imageUrl = video.snippet.thumbnails.default.url;
+const VideoListItem = (props) => {
+  const imageUrl = props.video.snippet.thumbnails.default.url;
 
   return (
     <li className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
-          <img className="media-object" src={"imageUrl"} />
+          <img className="media-object" src={imageUrl} />
         </div>
         <div className="media-body">
-          <div className="media-heading">{"video.snippet.title"}</div>
+          <div className="media-heading">{props.video.snippet.title}</div>
         </div>
       </div>
     </li>
